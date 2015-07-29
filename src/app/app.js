a7e8313fe30b76a6852e7493f8506c2f6254743b
Andrew <andriy.kemin@kemin.gmail.com>
2015-07-29 16:55:26 +0300

    1. home module - load slides data from external json.

--- src/app/app.js
+++ src/app/app.js
@@ -1,118 +1,123 @@
-(function () {
-    angular.module('photo-state', [
-        'ngMessages',
-        'ngAnimate',
-        'templates-app',
-        'templates-common',
-        'ui.router',
-        'photo-state.home',
-        'photo-state.signup',
-        'photo-state.signin',
-        'photo-state.help',
-        'photo-state.navigation',
-        'photo-state.constants',
-        'photo-state.common-validators',
-        'photo-state.recaptcha',
-        'photo-state.terms-and-conditions',
-        'photo-state.feedbacks',
-        'ui.bootstrap.showErrors',
-        'ui.bootstrap',
-        'restangular',
-        'angularSpinner',
-        'ngTouch'
-    ])
+angular.module('photo-state', [
+    'ngMessages',
+    'ngAnimate',
+    'templates-app',
+    'templates-common',
+    'ui.router',
+    'photo-state.home',
+    'photo-state.signup',
+    'photo-state.signin',
+    'photo-state.help',
+    'photo-state.navigation',
+    'photo-state.constants',
+    'photo-state.common-validators',
+    'photo-state.recaptcha',
+    'photo-state.terms-and-conditions',
+    'photo-state.feedbacks',
+    'ui.bootstrap.showErrors',
+    'ui.bootstrap',
+    'restangular',
+    'angularSpinner',
+    'ngTouch'
+])
 
-        .config(['$stateProvider', '$urlRouterProvider', 'showErrorsConfigProvider', 'RestangularProvider', function ($stateProvider, $urlRouterProvider, showErrorsConfigProvider, RestangularProvider) {
-            $urlRouterProvider.otherwise('/home');
-            showErrorsConfigProvider.showSuccess(true);
+    .config(['$stateProvider', '$urlRouterProvider', 'showErrorsConfigProvider', 'RestangularProvider', function ($stateProvider, $urlRouterProvider, showErrorsConfigProvider, RestangularProvider) {
+        $urlRouterProvider.otherwise('/home');
+        showErrorsConfigProvider.showSuccess(true);
 
-            RestangularProvider.setBaseUrl('http://demo5139190.mockable.io/');
-            RestangularProvider.setDefaultHttpFields({cache: false});
-        }])
+        RestangularProvider.setBaseUrl('http://demo5139190.mockable.io/');
+        RestangularProvider.setDefaultHttpFields({cache: false});
+    }])
 
-        .run(['$rootScope', '$log', 'Restangular', function run($rootScope, $log, Restangular) {
+    .run(['$rootScope', '$log', 'Restangular', function run($rootScope, $log, Restangular) {
 
-            $rootScope.serviceError = {};
-            Restangular.setErrorInterceptor(
-                function (response) {
-                    if (response.status != 200) {
-                        var message = '';
-                        if (response.status == 401) {
-                            message = 'Login required';
-                        } else if (response.status == 404) {
-                            message = 'Resource not available';
-                        } else {
-                            message = "Response received with HTTP error code: " + response.status;
-                        }
-
-                        $rootScope.serviceError.displayMessage = message;
-                        $log.error(message);
-                        //return false; // stop the promise chain
+        $rootScope.serviceError = {};
+        Restangular.setErrorInterceptor(
+            function (response) {
+                if (response.status != 200) {
+                    var message = '';
+                    if (response.status == 401) {
+                        message = 'Login required';
+                    } else if (response.status == 404) {
+                        message = 'Resource not available';
+                    } else {
+                        message = "Response received with HTTP error code: " + response.status;
                     }
+
+                    $rootScope.serviceError.displayMessage = message;
+                    $log.error(message);
+                    //return false; // stop the promise chain
                 }
-            );
-        }])
+            }
+        );
+    }])
 
-        .controller('AppCtrl', ['$scope', '$location', 'appConfig', '$modal', AppCtrl]);
+    .controller('AppCtrl', ['$scope', '$location', 'appConfig', '$modal', AppCtrl])
 
-    function AppCtrl($scope, $location, appConfig, $modal) {
+    .factory('LocalRestangular', ['Restangular', LocalRestangular]);
 
-        var self = this;
+function AppCtrl($scope, $location, appConfig, $modal) {
 
-        init();
+    var self = this;
 
-        this.openModalWindow = openModalWindow;
-        this.openTermsAndConditionsDialog = openTermsAndConditionsDialog;
-        this.pageSysName = null;
+    init();
 
-        function init() {
-            self.pageSysName = self.pageSysName || 'home';
-            $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
-                if (angular.isDefined(toState.data.pageTitle)) {
-                    $scope.pageTitle = toState.data.pageTitle + ' | Photo-State';
-                    self.pageSysName = toState.data.name;
+    this.openModalWindow = openModalWindow;
+    this.openTermsAndConditionsDialog = openTermsAndConditionsDialog;
+    this.pageSysName = null;
+
+    function init() {
+        self.pageSysName = self.pageSysName || 'home';
+        $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
+            if (angular.isDefined(toState.data.pageTitle)) {
+                $scope.pageTitle = toState.data.pageTitle + ' | Photo-State';
+                self.pageSysName = toState.data.name;
+            }
+        });
+
+        self.INPUT_MAX_CHARS = appConfig.inputMaxChars;
+    }
+
+    function openModalWindow(options) {
+        var modalInstance = $modal.open({
+            animation: options.animationsEnabled || true,
+            templateUrl: options.templateUrl,
+            controller: options.controller,
+            controllerAs: options.controllerAs,
+            size: options.size || 'md',
+            resolve: {
+                items: function () {
+                    return $scope.items;
                 }
-            });
+            }
+        });
 
-            self.INPUT_MAX_CHARS = appConfig.inputMaxChars;
+        if (options.okHandler) {
+            modalInstance.result.success(options.okHandler);
         }
 
-        function openModalWindow(options) {
-            var modalInstance = $modal.open({
-                animation: options.animationsEnabled || true,
-                templateUrl: options.templateUrl,
-                controller: options.controller,
-                controllerAs: options.controllerAs,
-                size: options.size || 'md',
-                resolve: {
-                    items: function () {
-                        return $scope.items;
-                    }
-                }
-            });
-
-            if (options.okHandler) {
-                modalInstance.result.success(options.okHandler);
-            }
-
-            if (options.cancelHandler) {
-                modalInstance.result.error(options.cancelHandler);
-            }
-
-            if (options.finalHandler) {
-                modalInstance.result.finally(options.finalHandler);
-            }
+        if (options.cancelHandler) {
+            modalInstance.result.error(options.cancelHandler);
         }
 
-        function openTermsAndConditionsDialog() {
-            openModalWindow({
-                animationsEnabled: true,
-                templateUrl: 'terms-and-conditions/terms-and-conditions.tpl.html',
-                controller: 'TermsAndConditionsCtrl',
-                controllerAs: 'termsCtrl'
-            });
+        if (options.finalHandler) {
+            modalInstance.result.finally(options.finalHandler);
         }
     }
-})
-();
 
+    function openTermsAndConditionsDialog() {
+        openModalWindow({
+            animationsEnabled: true,
+            templateUrl: 'terms-and-conditions/terms-and-conditions.tpl.html',
+            controller: 'TermsAndConditionsCtrl',
+            controllerAs: 'termsCtrl'
+        });
+    }
+}
+
+function LocalRestangular(Restangular) {
+    return Restangular.withConfig(function (RestangularConfigurer) {
+        RestangularConfigurer.setBaseUrl('.');
+        RestangularConfigurer.setRequestSuffix('.json');
+    });
+}
--- src/app/home/home.js
+++ src/app/home/home.js
@@ -9,27 +9,43 @@ angular.module('photo-state.home', [
             controller: 'HomeCtrl',
             controllerAs: 'homeCtrl',
             templateUrl: 'home/home.tpl.html',
-            data: {pageTitle: 'Home', name: 'home'}
+            data: {pageTitle: 'Home', name: 'home'},
+            resolve: {
+                pageData: ['$stateParams', 'HomeService', function ($stateParams, HomeService) {
+                    return HomeService.getPageContent();
+                }]
+            }
         });
     }])
 
-    .controller('HomeCtrl', ['$scope', HomeCtrl]);
+    .factory('HomeService', ['Restangular', 'LocalRestangular', HomeService])
+
+    .controller('HomeCtrl', ['$scope', 'pageData', HomeCtrl]);
 
 
-function HomeCtrl($scope) {
+function HomeCtrl($scope, pageData) {
 
     var self = this;
 
     this.myInterval = 5000;
     this.noWrapSlides = false;
     this.slides = [];
+    this.pageData = pageData;
 
     init();
 
     function init() {
-        self.slides.push({text: 'Slide 1 Title', logo: 'https://placekitten.com/606/300'});
-        self.slides.push({text: 'Slide 2 Title', logo: 'https://placekitten.com/606/300'});
-        self.slides.push({text: 'Slide 3 Title', logo: 'https://placekitten.com/606/300'});
+    }
+
+}
+
+function HomeService(Restangular, LocalRestangular) {
+    return {
+        getPageContent: getPageContent
+    };
+
+    function getPageContent() {
+        return LocalRestangular.one('assets/slides/slides-data.json').customGET();
     }
 
 }
